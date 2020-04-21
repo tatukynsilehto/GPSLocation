d8b56f8d706330030b71ef8792f719c787c8fdf9
tatukynsilehto <46740607+tatukynsilehto@users.noreply.github.com>
2020-04-22 00:25:12 +0300

    parempi

--- app/src/main/java/com/example/gpslocation/MainActivity.java
+++ app/src/main/java/com/example/gpslocation/MainActivity.java
@@ -1,44 +1,93 @@
 package com.example.gpslocation;
 
-import android.location.Location;
-import android.os.Bundle;
-import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.EditText;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import android.location.Geocoder;
+import android.widget.TextView;
 
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    TextView textView;
-    private static DecimalFormat df = new DecimalFormat("0.00");
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView coordinates;
+    private TextView address;
+    private double lat = 0.0, lon = 0.0;
+    private int locationRequestCode = 1000;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fusedLocationProviderClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null){
-                            textView = findViewById(R.id.Text);
-                            double longitude = location.getLongitude();
-                            double latitude = location.getLatitude();
-                            String Longitude = (df.format(longitude));
-                            String Latitude = (df.format(latitude));
-                            textView.setText(Longitude + "," + Latitude);
+
+        coordinates = (TextView) findViewById(R.id.coordinates);
+        address = (TextView) findViewById(R.id.address);
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+
+        } else {
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+                @Override
+                public void onSuccess(Location location) {
+
+                    if (location != null) {
+                        lat = location.getLatitude();
+                        lon = location.getLongitude();
+                        coordinates.setText(String.format(Locale.US, "%s, %s", lat, lon));
+                        Geocoder geocoder = new Geocoder(getApplicationContext(),Locale.getDefault());
+                        try{
+                            List<Address>listAddresses = geocoder.getFromLocation(lat,lon,1);
+                            if(null!=listAddresses&&listAddresses.size()>0){
+                                String _Location = listAddresses.get(0).getAddressLine(0);
+                                address.setText(_Location);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
+
+
+
+
+                    } else {
+                        coordinates.setText("asd");
                     }
-                });
+                }
 
 
+
+
+
+
+            });
+        }
 
     }
+
 }
